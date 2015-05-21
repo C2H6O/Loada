f5c062c2056d556e165d3beef1ae83f2ceb8fc70
Alexandre Doubov <alexandre.doubov@gmail.com>
2015-05-21 13:23:35 -0400

    Created ViewState to save/restore fragment's UI properly upon rotation

--- loada/src/main/java/net/doubov/loada/LoadaLceFragment.java
+++ loada/src/main/java/net/doubov/loada/LoadaLceFragment.java
@@ -3,13 +3,20 @@ package net.doubov.loada;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.view.ViewStub;
 import android.widget.TextView;
 
 public abstract class LoadaLceFragment<CV extends View, M> extends LoadaBaseFragment implements LoadaLceView<M> {
+
+    public static final String TAG = LoadaLceFragment.class.getSimpleName();
+
+    public static final String STATE_VIEW_STATE = TAG + "STATE_VIEW_STATE";
+
     protected View mLoadingView;
     protected CV mContentView;
     protected TextView mErrorView;
     protected boolean mAnimateChanges = true;
+    private ViewState mViewState = ViewState.LOADING;
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -42,8 +49,40 @@ public abstract class LoadaLceFragment<CV extends View, M> extends LoadaBaseFrag
                 onErrorViewClicked();
             }
         });
-        onLceViewsCreated();
 
+        if (savedInstanceState != null) {
+            mViewState = (ViewState) savedInstanceState.getSerializable(STATE_VIEW_STATE);
+            restoreViewState(mViewState);
+        }
+
+        onLceViewsCreated();
+    }
+
+    private void restoreViewState(ViewState state) {
+        switch (state) {
+            case CONTENT:
+                showContent();
+                break;
+            case LOADING:
+                showLoading();
+                break;
+            case ERROR:
+                showError();
+                break;
+        }
+    }
+
+    private enum ViewState {
+        LOADING,
+        CONTENT,
+        ERROR
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putSerializable(STATE_VIEW_STATE, mViewState);
     }
 
     protected void onLceViewsCreated() {
@@ -58,15 +97,18 @@ public abstract class LoadaLceFragment<CV extends View, M> extends LoadaBaseFrag
 
     @Override
     public void showLoading() {
+        mViewState = ViewState.LOADING;
         LoadaLceAnimator.showLoading(mLoadingView, mContentView, mErrorView);
     }
     @Override
     public void showContent() {
+        mViewState = ViewState.CONTENT;
         LoadaLceAnimator.showContent(mAnimateChanges, mLoadingView, mContentView, mErrorView);
     }
 
     @Override
     public void showError() {
+        mViewState = ViewState.ERROR;
         LoadaLceAnimator.showErrorView(mAnimateChanges, mLoadingView, mContentView, mErrorView);
     }
 
